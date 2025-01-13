8ef7ce346fefe23c28e759f1927b763300030703
Yuliya Bezlatneva <bezlatneva@yandex.com>
2025-01-13 21:29:07 +0700

    fix: replaced tests for get sync task by id into new class

--- src/main/test/java/api/sync_controller/GetSyncTaskListTest.java
+++ src/main/test/java/api/sync_controller/GetSyncTaskListTest.java
@@ -3,33 +3,19 @@ package api.sync_controller;
 import base.BaseTest;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Description;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+import io.qameta.allure.Feature;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
-import static io.restassured.http.ContentType.JSON;
-import static org.apache.http.HttpHeaders.CONTENT_TYPE;
 
 public class GetSyncTaskListTest extends BaseTest {
 
-    private static String sessionCookie;
-    private static final int syncTaskId = 1010;
-    private static final int wrongSyncTaskId = 0;
-
-    @BeforeAll
-    public static void authenticate() {
-        sessionCookie = given()
-                .contentType(ContentType.JSON)
-                .body("{ \"username\": \"test\", \"password\": \"test\" }")
-                .post(RestAssured.baseURI + "api/v1" + "/auth/login")
-                .then()
-                .statusCode(200)
-                .extract()
-                .cookie("JSESSIONID");
-        System.out.println("Authenticated. Session Cookie: " + sessionCookie);
-    }
+    @Feature("Get sync task list")
 
     @Test
     @Tag("positive")
@@ -37,71 +23,41 @@ public class GetSyncTaskListTest extends BaseTest {
     @DisplayName("Get Sync Task List successfully")
     @Description("Get synchronization task list with status code 200")
     public void verifyStatusCodeForGetSyncTaskList() {
+        System.out.println("--- Start test: Get Sync Task List successfully");
+        Allure.step("Sending get sync task list request.");
         Response response = given()
-                .header(CONTENT_TYPE, JSON)
-                .header("Cookie", "JSESSIONID=" + sessionCookie)
+                .header("Content-Type", "application/json")
+                .header("Cookie", "JSESSIONID=" + authCookie)
                 .log().all()
                 .when()
-                .get(RestAssured.baseURI + "api/v1/sync");
-        Allure.step("Verify status code is 200");
-        response.then()
-                .log().all()
-                .statusCode(200);
-    }
+                .get(baseURI + "/api/v1/sync");
 
-    @Test
-    @Tag("positive")
-    @Order(2)
-    @DisplayName("Get Sync Task by id successfully")
-    @Description("Get synchronization task by id with status code 200")
-    public void getSyncTaskByIdSuccessfully() {
-        Response response = given()
-                .header(CONTENT_TYPE, JSON)
-                .header("Cookie", "JSESSIONID=" + sessionCookie)
-                .log().all()
-                .when()
-                .get(RestAssured.baseURI + "api/v1/sync/" + syncTaskId);
-        Allure.step("Verify status code is 200");
-        response.then()
-                .log().all()
-                .statusCode(200);
+        System.out.println("--- Response Body: " + response.getBody().prettyPrint());
+        System.out.println("--- Checking response status code.");
+        Allure.step("Verify status-code is 200.");
+        response.then().statusCode(200);
+        System.out.println("--- Response code: " + response.getStatusCode());
     }
 
     @Test
     @Tag("negative")
-    @Order(3)
+    @Order(2)
     @DisplayName("Get 500 for Sync Task List")
     @Description("Get status code 500 for Sync Task List with wrong path")
     public void verifyStatusCodeForGetSyncTaskListWithWrongPath() {
+        System.out.println("--- Start test: Get 500 for Sync Task List");
+        Allure.step("Sending get sync task list request with wrong path.");
         Response response = given()
-                .header(CONTENT_TYPE, JSON)
-                .header("Cookie", "JSESSIONID=" + sessionCookie)
+                .header("Content-Type", "application/json")
+                .header("Cookie", "JSESSIONID=" + authCookie)
                 .log().all()
                 .when()
-                .get(RestAssured.baseURI + "api/v1/sinc");
-        Allure.step("Verify status code is 500");
-        response.then()
-                .log().all()
-                .statusCode(500);
-    }
+                .get(baseURI + "/api/v1/sinc"); // Wrong path
 
-    @Test
-    @Disabled("Comes status code 200 instead of 500!")
-    @Tag("negative")
-    @Order(4)
-    @DisplayName("Get 500 for Sync Task by id")
-    @Description("Get synchronization task by wrong id with status code 500")
-    public void verifyStatusCodeForWrongSyncTaskId() {
-        Response response = given()
-                .header(CONTENT_TYPE, JSON)
-                .header("Cookie", "JSESSIONID=" + sessionCookie)
-                .log().all()
-                .when()
-                .get(RestAssured.baseURI + "api/v1/sync/" + wrongSyncTaskId);
-        Allure.step("Verify status code is 500");
-        response.then()
-                .log().all()
-                .statusCode(500);
+        System.out.println("--- Response Body: " + response.getBody().prettyPrint());
+        System.out.println("--- Checking response status code.");
+        Allure.step("Verify status-code is 500.");
+        response.then().statusCode(500);
+        System.out.println("--- Response code: " + response.getStatusCode());
     }
-
 }
