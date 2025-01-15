db75d1b06b88698b2edb01320336dae9fba430f0
Anastasiia Kolesnik <kolesnik@farzoom.com>
2025-01-15 12:15:03 +0500

    changed rest controller

--- src/test/java/utils/RestClient.java
+++ src/test/java/utils/RestClient.java
@@ -14,10 +14,10 @@ public class RestClient {
         this.baseUri = baseUri;
     }
 
-    public Response get(String endpoint, Map<String, Object> queryParams, Map<String, String> headers, String cookie) {
+    public Response get(String endpoint, Map<String, Object> queryParams, String cookie) {
+
         return RestAssured.given()
                 .baseUri(baseUri)
-                .headers(headers)
                 .queryParams(queryParams)
                 .cookie("JSESSIONID", cookie)
                 .when()
@@ -35,10 +35,9 @@ public class RestClient {
                 .post(endpoint);
     }
 
-    public Response put(String endpoint, Object body, Map<String, String> headers, String cookie) {
+    public Response put(String endpoint, Object body, String cookie) {
         return RestAssured.given()
                 .baseUri(baseUri)
-                .headers(headers)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .cookie("JSESSIONID", cookie)
@@ -47,10 +46,9 @@ public class RestClient {
                 .put(endpoint);
     }
 
-    public Response delete(String endpoint, Map<String, String> headers, String cookie) {
+    public Response delete(String endpoint, String cookie) {
         return RestAssured.given()
                 .baseUri(baseUri)
-                .headers(headers)
                 .cookie("JSESSIONID", cookie)
                 .when()
                 .delete(endpoint);
